6ef564d0ebda9236f016cce0404eb47b951a9c40
olga-evdokimova <pavedom13@gmail.com>
2022-11-02 14:26:55 +0300

    Update 2022-11-02T11:26:52.641Z

--- js/app.js
+++ js/app.js
@@ -3474,6 +3474,15 @@
         beforeAfterSlider(x);
         pauseEvents(e);
     }));
+    const widgets = document.querySelectorAll(".widget");
+    widgets.forEach((function(widget) {
+        widget.addEventListener("click", (function(e) {
+            if (e.target.classList.contains("widget__title")) {
+                e.target.classList.toggle("widget__title--active");
+                e.target.nextElementSibling.classList.toggle("widget__body--hidden");
+            }
+        }));
+    }));
     window["FLS"] = true;
     isWebp();
     menuInit();
